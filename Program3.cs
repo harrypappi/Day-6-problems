44509ebbc12686ffbb0e441cb9ac21002e703a49
“reachsumit1” <harry.pappi143@gmail.com>
2023-06-03 22:25:10 +0530

    add:second

---
+++ Program3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class PrimeNumber
+{
+    public static bool IsPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+
+        if (number == 2)
+        {
+            return true;
+        }
+
+        if (number % 2 == 0)
+        {
+            return false;
+        }
+
+        int sqrt = (int)Math.Sqrt(number);
+
+        for (int divisor = 3; divisor <= sqrt; divisor += 2)
+        {
+            if (number % divisor == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsPrime(number))
+        {
+            Console.WriteLine(number + " is a prime number.");
+        }
+        else
+        {
+            Console.WriteLine(number + " is not a prime number.");
+        }
+    }
+}
