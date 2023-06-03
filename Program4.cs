7d31422029c57de99a3b49f1fda388db3adb66bd
“reachsumit1” <harry.pappi143@gmail.com>
2023-06-03 23:02:41 +0530

    Add:Program4

---
+++ Program4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class ReverseNumber
+{
+    public static int Reverse(int number)
+    {
+        int reverse = 0;
+        while (number != 0)
+        {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number /= 10;
+        }
+        return reverse;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int reversedNumber = Reverse(number);
+
+        Console.WriteLine("Reversed number: " + reversedNumber);
+    }
+}
